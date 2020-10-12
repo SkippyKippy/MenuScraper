e98d310cab9f3c9bbe9980041d37c1d9fef960da
Kipras Januska <kiprasjanuska0@gmail.com>
2020-10-12 03:20:56 -0400

    ugly ass discord bot menu works

--- scrape.py
+++ scrape.py
@@ -1,13 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import discord
+from discord.ext import commands
+import asyncio
 
 BASE_URL = "https://dining.umich.edu/menus-locations/dining-halls/"
 MOSHER = BASE_URL + "mosher-jordan/"
 MARKLEY = BASE_URL + "markley/"
 
+token = "NzY1MDk2MTM5OTc1NTU3MTQw.X4P1Tg.oVQBO8nZ5wVHRG2mq9fuQHMgofQ"
+client = commands.Bot(command_prefix="?")
 
-def get_meals(selections):
+
+async def get_meals(selections):
     foodItems = []
     selections = selections.find("ul", class_="courses_wrapper").find_all(
         "li", recursive=False
@@ -26,7 +31,7 @@ def get_meals(selections):
     return foodItems
 
 
-def get_menu(location):
+async def get_menu(location):
     response = requests.get(location)
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -43,10 +48,32 @@ def get_menu(location):
     for index, course in enumerate(all_courses):
         tempArray = []
         tempArray.append(mealNames[index].text.strip())
-        tempArray.append(get_meals(course))
+        tempArray.append(await get_meals(course))
         meals.append(tempArray)
 
     return meals
 
 
-print(get_menu(MARKLEY))
+@client.command(pass_context=True)
+async def m(ctx, *args):
+    food = []
+    # if args == "mojo":
+    #     food = await get_menu(MOSHER)
+    # elif args == "markley":
+    #     food = await get_menu(MARKLEY)
+    food = await get_menu(MOSHER)
+    embed = discord.Embed(
+        title="menu",
+        color=0x099F5F,
+    )
+    for courses in food:
+        embed.add_field(name=f"__{courses[0]}__", value="‎⠀", inline=False)
+        for course in courses[1]:
+            food_items = ""
+            for food_item in course[1]:
+                food_items += food_item + "\n"
+            embed.add_field(name=course[0], value=food_items, inline=False)
+    await ctx.send(embed=embed)
+
+
+client.run(token)
